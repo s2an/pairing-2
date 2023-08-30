5de08ade5fbbee7d820d9f6482b9e6faa36ce5a5
Stephen Nash <stephen.aa.nash@gmail.com>
2023-08-30 10:59:47 -0400

    Add arrays

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ arrays = ["Jeff", "Jake", "John", "Jon", "Jonathan"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+places = ["Bahamas", "Japan", "Disneyland", "Canada", "France"]
+puts places.pop
